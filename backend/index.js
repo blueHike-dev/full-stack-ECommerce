cd06c17ab9f2c41de3e4a68f963334323a807bbc
simo <wambuisimon712@gmail.com>
2023-10-20 12:01:19 +0300

    noon

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
