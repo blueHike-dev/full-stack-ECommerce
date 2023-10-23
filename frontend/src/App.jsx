0c1a2ad5cea63884cee1df1bd381f2baf7077e32
simo <wambuisimon712@gmail.com>
2023-10-23 20:16:28 +0300

    good night

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,28 @@ import "./App.css";
 import CategoryItem from "./components/CategoryItem";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
-// import Register from "./pages/Register";
+import Register from "./pages/Register";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//  return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/blog/*" element={<BlogApp />} />
+//         <Route path="/users/*" element={<UserApp />} />
+//       </Routes>
+//     </BrowserRouter>
 
 function App() {
   return (
-    <>
-      <Cart />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
